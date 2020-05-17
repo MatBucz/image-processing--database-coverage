d896a1705a804f2328978171a75c3a15e4583143
Mateusz Buczkowski <buczkowski.mateusz@gmail.com>
2020-05-17 22:05:03 +0200

    Add tests to pre-commit and update tests

--- tests/unit/test_database_metrics.py
+++ tests/unit/test_database_metrics.py
@@ -5,7 +5,7 @@ from app.database_metrics import DatabaseMetrics, DatabaseMetricsError
 
 class TestDatabaseMetrics(TestCase):
     def test_should_plot_all(self):
-        dm = DatabaseMetrics("tests/assets/test_db", None, "test_db")
+        dm = DatabaseMetrics("tests/assets/test_db", None, (100, 100), "test_db")
         dm.plot_all()
 
     def test_should_raise_on_missing_dir(self):
